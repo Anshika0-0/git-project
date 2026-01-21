8cb55a959c38f948344ddd74d14ca350885aa1f4
Anshika0-0 <24BCS10680@cuchd.in>
2026-01-21 22:05:00 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 class Main {
     public static void main(String[] args) {
 
-        int a = 10;
-        int b = 20;
+        int b = 10;
+        int c = 20;
 
-        int sum = a + b;
-
+        int sum = b + c;
         System.out.println("Sum = " + sum);
     }
 }
